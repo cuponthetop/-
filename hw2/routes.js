2043e3f661f8c1abf23b6638c7562872eb96bacb
luffy-papi <zlee87@snu.ac.kr>
2016-01-27 02:26:02 -0800

    404

--- hw2/routes.js
+++ hw2/routes.js
@@ -18,6 +18,10 @@ router.get('/success', function (req, res) {
 	res.render('success');
 });
 
+router.get('*', function (req, res) {
+	res.send('404 Page Not Found');
+});
+
 router.post('/users', function (req, res) {
 	console.log(req.body.username);
 	res.redirect('success');
